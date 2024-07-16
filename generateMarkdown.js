abd9850c2737b9e7a3f87be156d4772416b810ca
elissegomez24 <elissegomez24@gmail.com>
2024-07-16 13:31:15 -0400

    Updated challenge files

---
+++ generateMarkdown.js
@@ -0,0 +1,77 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  }
+  return `[MIT License](license.txt)`;
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  }
+  return `[${license}][MIT License](license.txt)`;
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+  return `## License
+
+This project is licensed under the ${license} [License](license.txt)`;
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+  ${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+For any questions, please contact me with the information below:
+
+GitHub: [${data.github}](https://github.com/${data.github})  
+Email: ${data.email}
+`;
+}
+
+module.exports = generateMarkdown;
